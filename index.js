b65ea1ff26af2bcfd7ae8ffff8d9cd76195010d6
conorhastings <conor@socialtables.com>
2016-11-01 09:56:18 -0400

    make keys matter in inner objects

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function getObjectValues(obj, keysToIgnore) {
   });
 }
 
+function filterObject(obj, keysToIgnore) {
+  return Object.keys(obj).reduce(function(newObj, key) {
+    if (keysToIgnore.indexOf(key) === -1) {
+      newObj[key] = obj[key];
+    }
+    return newObj;
+  }, {});
+}
+
 function hasSameType(a, b) {
   var bothArray = Array.isArray(a) && Array.isArray(b);
   var bothNull = a === null && b === null;
@@ -27,18 +36,21 @@ function shouldTripleEquals(item) {
   );
 }
 
+function valueEquality(a, b, opts) {
+  if (!hasSameType(a, b)) {
+    return false;
+  } else if (shouldTripleEquals(a)) {
+    return a === b;
+  } else if (Array.isArray(a)) {
+    return arrayEquality(a, b, opts);
+  } else if (typeof a === 'object') {
+    return objectEquality(a, b, opts);
+  }
+}
 function equality(a, b, opts) {
   return a.every(function(aValue) {
     return b.some(function(bValue) {
-      if (!hasSameType(aValue, bValue)) {
-        return false;
-      } else if (shouldTripleEquals(aValue)) {
-        return aValue === bValue;
-      } else if (Array.isArray(aValue)) {
-        return arrayEquality(aValue, bValue, opts);
-      } else if (typeof aValue === 'object') {
-        return objectValueEquality(aValue, bValue, opts);
-      }
+      return valueEquality(aValue, bValue, opts);
     });
   });
 }
@@ -50,15 +62,26 @@ function arrayEquality(a, b, opts) {
   return equality(a, b, opts);
 }
 
+function objectEquality(a, b, opts) {
+  var filteredObjectA = filterObject(a, opts.keysToIgnore);
+  var filteredObjectB = filterObject(b, opts.keysToIgnore);
+  if (Object.keys(filteredObjectA).length !== Object.keys(filteredObjectB).length) {
+    return false;
+  }
+  return Object.keys(filteredObjectA).every(function(key) {
+    return valueEquality(filteredObjectA[key], filteredObjectB[key], opts);
+  });
+}
+
 function objectValueEquality(a, b, opts) {
   opts = opts || {};
-  var keysToIgnore = opts.keysToIgnore || [];
+  opts.keysToIgnore = opts.keysToIgnore || [];
   /* we can return early if number of keys is not equal */
   if (Object.keys(a).length !== Object.keys(b).length) {
     return false;
   }
-  var aValues = getObjectValues(a, keysToIgnore);
-  var bValues = getObjectValues(b, keysToIgnore);
+  var aValues = getObjectValues(a, opts.keysToIgnore);
+  var bValues = getObjectValues(b, opts.keysToIgnore);
   return equality(aValues, bValues, opts);
 }
 
